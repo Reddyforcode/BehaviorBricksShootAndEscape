48292477ab4b72f63ac56b023e33b4e23d05d3ae
ReddyTintaya <aaabeeelooon@gmail.com>
2018-09-18 23:26:20 -0400

    first

---
+++ New Unity Project (2)/Assets/BehaviorBricks/Conditions/System/Perception/HasBullets.cs
@@ -0,0 +1,20 @@
+﻿using Pada1.BBCore;
+using UnityEngine;
+namespace BBUnity.Conditions
+{
+	
+	[Condition("Perception/HasBullets")]
+	[Help("Checks if the enemy still has Bullets")]
+
+	public class HasBullets : GOCondition
+	{
+		
+		
+        public override bool Check()
+        {
+			int a = GameObject.FindWithTag("tagEnemy").GetComponent<numberBullets>().bullets;
+			return !(a>=0);
+        }
+    }
+}
+
---
+++ New Unity Project (2)/Assets/BehaviorBricks/Samples/ProgrammersQuickStartGuide/Done/DoneBulletScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Component used to destroy itself, in this case for the bullets.
+/// </summary>
+public class DoneBulletScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    /// <summary>
+    /// Initialize the component to be destroy the GameObject in 2 seconds.
+    /// </summary>
+    void Start()
+	{
+        GameObject.FindWithTag("tagEnemy").GetComponent<numberBullets>().bullets--;
+
+		Destroy(gameObject, 2);
+	} // Start
+
+}
